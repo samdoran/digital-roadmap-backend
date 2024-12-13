44f83bfb7ce6395102908262308a8daac3b637ed
Sam Doran <sdoran@redhat.com>
2024-12-13 16:24:12 -0500

    Add environment variables for database connection parameters (#7)

--- app/config.py
+++ app/config.py
@@ -5,4 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.getenv("SQLALCHEMY_DB", "release_notes_db")
 DB_USER = os.getenv("SQLALCHEMY_USER", "postgres")
 DB_PASSWORD = os.getenv("SQLALCHEMY_PASSWORD", "password")
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}"
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", 5432)
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
