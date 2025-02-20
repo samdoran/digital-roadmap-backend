d5b4758cdd8e6aadf9b4bb407400038b9541123c
Preston Watson <prwatson@redhat.com>
2025-02-20 14:48:29 -0500

    Update upcoming and system mocked data (#43)

--- src/roadmap/data/upcoming.py
+++ src/roadmap/data/upcoming.py
@@ -1,13 +1,13 @@
 UPCOMING_DATA = [
     {
-        "name": "OpenJDK 11 retirement",
-        "type": "Deprecation",
-        "release": "9.x",
+        "name": "Node.js 22 included in RHEL 9 Application Streams",
+        "type": "Addition",
+        "release": "9.5",
         "date": "Oct 2024",
         "details": {
             "detailFormat": 0,
-            "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub works as well!",
-            "potentiallyAffectedSystems": 5,
+            "summary": "RHEL 9.5 is tentatively planned to include Node.js 22. Nodejs 22 is currently available in RHEL's unsupported upstream distributions: CentOS Stream 9 and Fedora 41.  For more information on RHEL’s development process and upstream, see What is CentOS Stream?",
+            "potentiallyAffectedSystems": 25,
             "trainingTicket": "No training ticket",
             "dateAdded": "October 29, 2024",
             "lastModified": "October 29, 2024",
@@ -85,7 +85,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "gcc-toolset 14",
-        "type": "addition",
+        "type": "Addition",
         "release": "9.5",
         "date": "May 2025",
         "details": {
@@ -99,7 +99,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "gcc-toolset 14",
-        "type": "addition",
+        "type": "Addition",
         "release": "10.0",
         "date": "May 2025",
         "details": {
@@ -113,7 +113,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "MariaDB 11.0",
-        "type": "addition",
+        "type": "Addition",
         "release": "9.5",
         "date": "May 2025",
         "details": {
@@ -127,7 +127,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "MariaDB 11.0",
-        "type": "addition",
+        "type": "Addition",
         "release": "10.0",
         "date": "May 2025",
         "details": {
@@ -155,7 +155,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "PHP 8.3",
-        "type": "addition",
+        "type": "Addition",
         "release": "9.5",
         "date": "May 2025",
         "details": {
@@ -169,7 +169,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "PHP 8.3",
-        "type": "addition",
+        "type": "Addition",
         "release": "10.0",
         "date": "May 2025",
         "details": {
@@ -183,7 +183,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "Python 3.12",
-        "type": "addition",
+        "type": "Addition",
         "release": "9.5",
         "date": "May 2025",
         "details": {
@@ -197,7 +197,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "Python 3.12",
-        "type": "addition",
+        "type": "Addition",
         "release": "10.0",
         "date": "May 2025",
         "details": {
@@ -211,7 +211,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "Ruby 3.4",
-        "type": "addition",
+        "type": "Addition",
         "release": "9.5",
         "date": "May 2025",
         "details": {
@@ -225,7 +225,7 @@ UPCOMING_DATA = [
     },
     {
         "name": "Ruby 3.5",
-        "type": "addition",
+        "type": "Addition",
         "release": "10.0",
         "date": "June 2025",
         "details": {
