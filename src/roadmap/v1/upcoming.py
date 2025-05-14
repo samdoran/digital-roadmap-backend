976020f4efb0e3de331fa7eac973f8cbed0881cb
Ellis Low <elow@redhat.com>
2025-05-14 18:52:28 -0400

    [RSPEED-1400] Potentially affected systems (#162)

--- src/roadmap/v1/upcoming.py
+++ src/roadmap/v1/upcoming.py
@@ -1,10 +1,12 @@
 import typing as t
 
+from collections import defaultdict
 from datetime import date
 from enum import auto
 from enum import StrEnum
 from functools import lru_cache
 from pathlib import Path
+from uuid import UUID
 
 from fastapi import APIRouter
 from fastapi import Depends
@@ -16,6 +18,8 @@ from pydantic import TypeAdapter
 from roadmap.common import ensure_date
 from roadmap.config import Settings
 from roadmap.models import Meta
+from roadmap.v1.lifecycle.app_streams import AppStreamKey
+from roadmap.v1.lifecycle.app_streams import systems_by_app_stream
 
 
 router = APIRouter(prefix="/upcoming-changes", tags=["Upcoming Changes"])
@@ -38,37 +42,87 @@ class Architecture(StrEnum):
     none = ""
 
 
-class UpcomingDetails(BaseModel):
+class UpcomingInputDetails(BaseModel):
     architecture: Architecture | None
     detailFormat: int
     summary: str
-    potentiallyAffectedSystems: int
     trainingTicket: str
     dateAdded: date = Field(default_factory=date.today)
     lastModified: Date
 
 
-class Upcoming(BaseModel):
+class UpcomingInput(BaseModel):
+    name: str
+    type: UpcomingType
+    package: str
+    release: str
+    date: Date
+    details: UpcomingInputDetails
+
+
+class UpcomingOutputDetails(BaseModel):
+    architecture: Architecture | None
+    detailFormat: int
+    summary: str
+    trainingTicket: str
+    dateAdded: date = Field(default_factory=date.today)
+    lastModified: Date
+    potentiallyAffectedSystemsCount: int
+    potentiallyAffectedSystems: list[UUID]
+
+
+class UpcomingOutput(BaseModel):
     name: str
     type: UpcomingType
     release: str
     date: Date
-    details: UpcomingDetails
+    details: UpcomingOutputDetails
 
 
 class WrappedUpcoming(BaseModel):
     meta: Meta
-    data: list[Upcoming]
+    data: list[UpcomingOutput]
 
 
 @lru_cache
-def read_upcoming_file(file: str | Path) -> list[Upcoming]:
-    with open(file, "r") as file:
-        return TypeAdapter(list[Upcoming]).validate_json(file.read())
+def read_upcoming_file(file: Path) -> list[UpcomingInput]:
+    return TypeAdapter(list[UpcomingInput]).validate_json(file.read_text())
 
 
-def get_upcoming_data(settings: t.Annotated[Settings, Depends(Settings.create)]) -> list[Upcoming]:
-    return read_upcoming_file(settings.upcoming_json_path)
+def get_upcoming_data(
+    systems_by_stream: t.Annotated[dict[AppStreamKey, list[UUID]], Depends(systems_by_app_stream)],
+    settings: t.Annotated[Settings, Depends(Settings.create)],
+) -> list[UpcomingOutput]:
+    keys_by_name = defaultdict(list)
+    [keys_by_name[a.name].append(a) for a in systems_by_stream.keys()]
+
+    result = []
+    for upcoming in read_upcoming_file(settings.upcoming_json_path):
+        systems = set()
+        for key in keys_by_name[upcoming.package]:
+            systems.update(systems_by_stream[key])
+
+        details = UpcomingOutputDetails(
+            architecture=upcoming.details.architecture,
+            detailFormat=upcoming.details.detailFormat,
+            summary=upcoming.details.summary,
+            trainingTicket=upcoming.details.trainingTicket,
+            dateAdded=upcoming.details.dateAdded,
+            lastModified=upcoming.details.lastModified,
+            potentiallyAffectedSystemsCount=len(systems),
+            potentiallyAffectedSystems=systems,
+        )
+
+        result.append(
+            UpcomingOutput(
+                name=upcoming.name,
+                type=upcoming.type,
+                release=upcoming.release,
+                date=upcoming.date,
+                details=details,
+            )
+        )
+    return result
 
 
 @router.get(
--- tests/v1/test_upcoming.py
+++ tests/v1/test_upcoming.py
@@ -1,21 +1,57 @@
 from pathlib import Path
 
+from roadmap.common import decode_header
+from roadmap.common import query_rbac
 from roadmap.config import Settings
 
 
 def test_get_upcoming_changes(client, api_prefix):
+    async def query_rbac_override():
+        return [
+            {
+                "permission": "inventory:*:*",
+                "resourceDefinitions": [],
+            }
+        ]
+
+    async def decode_header_override():
+        return "1234"
+
+    client.app.dependency_overrides = {}
+    client.app.dependency_overrides[query_rbac] = query_rbac_override
+    client.app.dependency_overrides[decode_header] = decode_header_override
+
     response = client.get(f"{api_prefix}/upcoming-changes")
     assert response.status_code == 200
-    assert response.json()["data"][0]["name"] == "New CLI experience for RHEL Image Builder"
+    data = response.json()["data"]
+    assert data[0]["name"] == "Add Node.js to RHEL9 AppStream THIS IS TEST DATA"
+    found_systems = False
+    for record in data:
+        if len(record["details"]["potentiallyAffectedSystems"]) > 1:
+            found_systems = True
+    assert found_systems
 
 
 def test_get_upcoming_changes_with_env(client, api_prefix):
     def settings_override():
         return Settings(upcoming_json_path=Path(__file__).parent.parent.joinpath("fixtures").joinpath("upcoming.json"))
 
+    async def query_rbac_override():
+        return [
+            {
+                "permission": "inventory:*:*",
+                "resourceDefinitions": [],
+            }
+        ]
+
+    async def decode_header_override():
+        return "1234"
+
     client.app.dependency_overrides = {}
     client.app.dependency_overrides[Settings.create] = settings_override
+    client.app.dependency_overrides[query_rbac] = query_rbac_override
+    client.app.dependency_overrides[decode_header] = decode_header_override
 
     response = client.get(f"{api_prefix}/upcoming-changes")
     assert response.status_code == 200
-    assert response.json()["data"][0]["name"] == "New CLI experience for RHEL Image Builder TEST"
+    assert response.json()["data"][0]["name"] == "Add Node.js to RHEL9 AppStream THIS IS TEST DATA TEST"
